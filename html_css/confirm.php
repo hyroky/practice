d4d34c1e753f1ce7db75d71377c3b492aae2ee67
hiroki.matsuo <neo.hiroki.matsuo@gmail.com>
2017-02-18 16:16:53 +0900

    注文の合計金額を出そう

--- html_css/confirm.php
+++ html_css/confirm.php
@@ -11,11 +11,15 @@
 <body>
   <div class="order-wrapper">
     <h2>注文内容確認</h2>
+    <!-- 変数$totalPaymentを定義し、数値の0を代入してください -->
+    <?php $totalPayment = 0; ?>
+    
     <?php foreach ($menus as $menu): ?>
       <?php 
         $orderCount = $_POST[$menu->getName()];
-        // $menuに対して、$orderCountを引数としてsetOrderCountメソッドを呼び出してください
         $menu->setOrderCount($orderCount);
+        // $totalPaymentに、$menuのgetTotalPriceメソッドで得た値を足してください
+        $totalPayment = $totalPayment + $menu->getTotalPrice();
         
       ?>
       <p class="order-amount">
@@ -24,9 +28,10 @@
         <?php echo $orderCount ?>
         個
       </p>
-      <!-- $menuに対してgetTotalPriceメソッドを呼び出して、金額を表示してください -->
-      <p class="order-price"><?php echo $menu->getTotalPrice(); ?>円</p>
+      <p class="order-price"><?php echo $menu->getTotalPrice() ?>円</p>
     <?php endforeach ?>
+    <!-- $totalPaymentを表示してください -->
+    <h3>合計金額: <?php echo $totalPayment; ?>円</h3>
   </div>
 </body>
 </html>
