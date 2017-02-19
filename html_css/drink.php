1076c34f234814556aa07d3792645b8178e3f9ad
hiroki.matsuo <neo.hiroki.matsuo@gmail.com>
2017-02-19 15:21:25 +0900

    独自メソッドを定義

--- html_css/drink.php
+++ html_css/drink.php
@@ -1,9 +1,19 @@
 <?php 
-// menu.phpを読み込んでください
-require_once ('menu.php');
+require_once('menu.php');
 
-// Menuクラスを継承したDrinkクラスを作成してください
 class Drink extends Menu {
+  // $typeというprivateなプロパティを定義してください
+  private $type;
+  
+  // getTypeメソッドを定義してください
+  public function getType(){
+    return $this->type;
+  }
+  
+  // setTypeメソッドを定義してください
+  public function setType($type){
+    return $this->type=$type;
+  }
   
 }
 
