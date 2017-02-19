46e5fa3ca85a4c254f43b1d56b35d25d2fae033c
hiroki.matsuo <neo.hiroki.matsuo@gmail.com>
2017-02-19 16:38:39 +0900

    display_review

--- html_css/show.php
+++ html_css/show.php
@@ -1,12 +1,8 @@
 <?php
-// menu.phpを読み込んでください
-require_once ('menu.php');
-
-// data.phpを読み込んでください
-require_once ('data.php');
+require_once('menu.php');
+require_once('data.php');
 
 $menuName = $_GET['name'];
-// Menuクラスに対してfindByNameというクラスメソッドを呼び出してください
 $menu = Menu::findByName($menus, $menuName);
 
 ?>
@@ -20,21 +16,35 @@ $menu = Menu::findByName($menus, $menuName);
   <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
 </head>
 <body>
-  <div class="review-wrapper container">
-    <!-- ソースコードを貼り付けてください -->
-    <img src="<?php echo $menu->getImage() ?>" class="menu-item-image">
-    <h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
-
-    <?php if ($menu instanceof Drink): ?>
-      <p class="menu-item-type"><?php echo $menu->getType() ?></p>
-    <?php else: ?>
-      <?php for ($i = 0; $i < $menu->getSpiciness(); $i++): ?>
-        <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/chilli.png" class='icon-spiciness'>
-      <?php endfor ?>
-    <?php endif ?>
-    <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?></p>
-    <a href="index.php">← メニュー一覧へ</a>
+  <div class="review-wrapper">
+    <div class="review-menu-item">
+      <img src="<?php echo $menu->getImage() ?>" class="menu-item-image">
+      <h3 class="menu-item-name"><?php echo $menu->getName() ?></h3>
+  
+      <?php if ($menu instanceof Drink): ?>
+        <p class="menu-item-type"><?php echo $menu->getType() ?></p>
+      <?php else: ?>
+        <?php for ($i = 0; $i < $menu->getSpiciness(); $i++): ?>
+          <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/chilli.png" class='icon-spiciness'>
+        <?php endfor ?>
+      <?php endif ?>
+      <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?></p>
+    </div>
     
+    <div class="review-list-wrapper">
+      <div class="review-list">
+        <div class="review-list-title">
+          <img src="https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/review.png" class='icon-review'>
+          <h4>レビュー一覧</h4>
+        </div>
+        <!-- foreach文を書いてください -->
+        <?php foreach($reviews as $review): ?>
+        	<h3><?php echo $review->getMenuName()?></h3>
+        	<p><?php echo $review->getBody()?></p>
+        <?php endforeach?>
+      </div>
+    </div>
+    <a href="index.php">← メニュー一覧へ</a>
   </div>
 </body>
 </html>
